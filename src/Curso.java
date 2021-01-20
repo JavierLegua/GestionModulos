499def0e53a2965a0de790cab9898e494bd1d81a
JAVIER LEGUA <javierlegua14@gmail.com>
2021-01-20 14:24:29 +0100

    Inicio v3.0

---
+++ src/Curso.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class Curso {
+
+	private String nombre, tutor;
+	private Modulos vModulos[];
+
+	public Curso(String nombre, String tutor) {
+		this.nombre = nombre;
+		this.tutor = tutor;
+		this.vModulos = new Modulos[10];
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTutor() {
+		return tutor;
+	}
+
+	public void setTutor(String tutor) {
+		this.tutor = tutor;
+	}
+
+	public Modulos[] getvModulos() {
+		return vModulos;
+	}
+
+	public void setvModulos(Modulos[] vModulos) {
+		this.vModulos = vModulos;
+	}
+
+	@Override
+	public String toString() {
+        String datos ="";
+        for (Modulos modulos : vModulos)
+        if(modulos!=null){
+            datos+= modulos.toString() + "\n";
+        }
+
+
+        return  nombre +"\n" + tutor + " \nMódulos\n------------\n" + datos;
+    }
+
+}
--- src/PrincipalModulos.java
+++ src/PrincipalModulos.java
@@ -1,27 +1,44 @@
 import java.util.Scanner;
 
+
+
 public class PrincipalModulos {
 
+	public static int pintaMenu() {
+		Scanner leer = new Scanner(System.in);
+		int opc = 0;
+		
+		do {
+			
+		System.out.println("Menú Gestión Curso");
+		System.out.println("1-Añadir un nuevo módulo");
+		System.out.println("2-Ver todos los módulos");
+		System.out.println("3-Ver horas del curso");
+		System.out.println("4-Salir");
+		
+		try{
+		opc=leer.nextInt();
+		}catch (Exception e){
+			opc = 0;
+			leer = new Scanner(System.in);
+		}
+		
+		} while (opc<1||opc>4); {
+			return opc;	
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 
-		Modulos vModulos[] = new Modulos[7];
-		int aux = 0;
-
-		Modulos programacion = new Modulos("Programacion", "PRG", 325);
-		Modulos sistemas = new Modulos("Sistemas informaticos", "SI", 257);
-		Modulos bbdd = new Modulos("Bases de datos", "BBDD", 180);
-
-		vModulos[0] = programacion;
-		vModulos[1] = sistemas;
-		vModulos[2] = bbdd;
-
-		for (Modulos m : vModulos) {
-			if (m != null) {
-				aux += m.getNumHoras();
-				System.out.println(m.toString());
-			}
-		}
-		System.out.println("El número total de horas de los módulos es " + aux);
+		pintaMenu();
+		
+		/**
+		 * Curso c = new Curso("1ºDAW", "Pepe");
+		 * System.out.println(c.toString());
+		 */
+		
+		
 	}
 
 }
