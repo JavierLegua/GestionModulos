802dbc2737bb8e08b5475319a8890973bb36f2fc
JAVIER LEGUA <javierlegua14@gmail.com>
2021-01-20 11:36:39 +0100

    Versión v1.0 terminada

--- src/Modulos.java
+++ src/Modulos.java
@@ -1,4 +1,48 @@
 
 public class Modulos {
 
+	private String nombre;
+	private String codigo;
+	private int numHoras;
+	
+	public Modulos(String nombre, String codigo, int numHoras) {
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.numHoras = numHoras;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumHoras() {
+		return numHoras;
+	}
+
+	public void setNumHoras(int numHoras) {
+		this.numHoras = numHoras;
+	}
+
+	@Override
+	public String toString() {
+		return "Modulos [nombre=" + nombre + ", codigo=" + codigo + ", numHoras=" + numHoras + "]";
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/PrincipalModulos.java
+++ src/PrincipalModulos.java
@@ -2,7 +2,14 @@
 public class PrincipalModulos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Modulos p = new Modulos("Programacion", "PRG", 300);
+		Modulos s = new Modulos("Sistemas informaticos", "SI", 300);
+		Modulos b = new Modulos("Bases de datos", "BBDD", 300);
+		
+		System.out.println(p.toString());
+		System.out.println(s.toString());
+		System.out.println(b.toString());
 
 	}
 
